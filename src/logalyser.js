5e057c6a3e886819965250b7b097ec1080d1fbff
yodalee <lc85301@gmail.com>
2018-10-07 23:17:19 +0800

    add js to handle file upload

--- src/logalyser.js
+++ src/logalyser.js
@@ -1,18 +1,27 @@
 "use strict";
+
 (function () {
 
 var Lglz = function () {
   console.log("constructor");
   this.content = "";
   this.filters = [];
+
   this.elem = this.createElement();
-  document.querySelector("body").appendChild(this.elem);
-}
+  document.querySelector("#logalyser").appendChild(this.elem);
+
+  var fileselect = this.elem.querySelector(".lglz-fileselect");
+  fileselect.addEventListener('change', this.handleFile.bind(this));
+};
 
 Lglz.prototype.template = [
   '<div class="lglz-top">',
-  '<textarea class="lglz-content">',
-  '</textarea>',
+    '<input type="file" class="lglz-fileselect"/>', '</br>',
+    '<textarea class="lglz-content">', '</textarea>',
+    '<div>',
+      'Filter',
+      '<div class="lglz-filter">', '</div>',
+    '</div>',
   '</div>',
 ].join("");
 
@@ -22,8 +31,38 @@ Lglz.prototype.createElement = function() {
   return div;
 }
 
-Lglz.prototype.setContent = function(content) {
+Lglz.prototype.loadContent = function(content) {
   this.content = content;
+  this.render();
+}
+
+Lglz.prototype.addFilter = function(keyword) {
+  this.filters.push(keyword);
+  this.render();
+}
+
+Lglz.prototype.render = function() {
+  var textarea = this.elem.querySelector('.lglz-content');
+  textarea.innerHTML = this.content;
+}
+
+Lglz.prototype.handleFile = function(evt) {
+  var file = evt.target.files[0];
+  var self = this;
+  // FIXME: how to deal with empty file extension
+  if (!(file.type.match("text/plain") || file.type == "")) {
+    return;
+  }
+
+  var reader = new FileReader();
+
+  reader.onload = (function(e) {
+    var content = e.target.result;
+    console.log(this);
+    this.loadContent(content);
+  }).bind(this);
+
+  reader.readAsText(file);
 }
 
 window.Lglz = Lglz;
@@ -31,5 +70,7 @@ window.Lglz = Lglz;
 })();
 
 window.onload = function() {(function () {
-  var lglz = new Lglz;
+
+var lglz = new Lglz;
+
 })()};
